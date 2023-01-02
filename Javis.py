14db4972c7b57595c54d3dc4b763c86e202349d9
TK17250 <94910246+TK17250@users.noreply.github.com>
2023-01-02 19:35:27 +0700

    first commit

---
+++ Javis.py
@@ -0,0 +1,42 @@
+import pyttsx3 as tx
+import speech_recognition as sr
+import os
+
+engine = tx.init()
+
+# Setting Speak
+TH = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_THAI" # เพิ่มเสียงภาษาไทย
+engine.setProperty("voice", TH)
+engine.setProperty('rate', 190)
+
+def speak(audio):
+    engine.say(audio) # ให้ Ai พูด
+    engine.runAndWait() # รอพูดจนจบ
+
+# การทำงาน
+def command():
+    r = sr.Recognizer() # รับค่าจากการฟัง
+
+    with sr.Microphone() as source:
+        print("กำลังฟัง...")
+        r.pause_threshold = 1 # รอคำสั่ง 1 วิ
+        r.adjust_for_ambient_noise(source, duration= 1) # ปรับเสียงรบกวน
+        audio = r.listen(source)
+
+    try: # รับค่าเสียงได้
+        print("รับค่าเสียง...")
+        query = r.recognize_google(audio, language='th-TH')
+        print(f"คุณพูดว่า >>> {query}\n")
+
+    except  Exception as e:
+        print(e)
+        print("คุณพูดใหม่ได้ไหม")
+        return  "None"
+    return query
+
+# การทำงาน
+while True:
+    query = command().lower()
+
+    if query in query:
+        speak(f"คุณพูดว่า >>> {query}")
